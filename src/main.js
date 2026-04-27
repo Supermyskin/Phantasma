77b8e0eef197a5f4caf763736ad57505f0595aa7
Supermyskin <supermyskin@abv.bg>
2026-04-27 22:03:00 +0300

    did lines and colored tips

--- src/main.js
+++ src/main.js
@@ -44,13 +44,31 @@ function renderLoop() {
     canvas.height = video.videoHeight;
     ctx.drawImage(video, 0, 0);
     if (detections.landmarks) {
+      const connections = [
+        [0, 1], [1, 2], [2, 3], [3, 4],
+        [0, 5], [5, 6], [6, 7], [7, 8],
+        [5, 9], [9, 10], [10, 11], [11, 12],
+        [9, 13], [13, 14], [14, 15], [15, 16],
+        [13, 17], [17, 18], [18, 19], [19, 20],
+        [0, 17]
+      ];
+
       for (const hand of detections.landmarks) {
-        for (const point of hand) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 2;
+        for (const [start, end] of connections) {
+          ctx.moveTo(hand[start].x * canvas.width, hand[start].y * canvas.height);
+          ctx.lineTo(hand[end].x * canvas.width, hand[end].y * canvas.height);
+        }
+        ctx.stroke();
+
+        hand.forEach((point, index) => {
           ctx.beginPath();
           ctx.arc(point.x * canvas.width, point.y * canvas.height, 5, 0, Math.PI * 2);
-          ctx.fillStyle = 'lime';
+          ctx.fillStyle = [4, 8, 12, 16, 20].includes(index) ? 'red' : 'lime';
           ctx.fill();
-        }
+        });
       }
     }
   }
